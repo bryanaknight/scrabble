c19ce1039e4ff9fd765310029e45098fc17bb4c7
Bryana Knight <brknig11@gmail.com>
2014-03-11 17:08:07 -0400

    updates

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,56 @@
+require 'pry'
+
 class Scrabble
+  # attr_reader :word
+
+  # def initialize(word)
+  #   @word = word
+  # end
+
+  # def self.score(word)
+  #   Scrabble.new(word).score
+  # end
+
+  # def self.highest_score_from(words)
+    # word_hash = words.each_with_object({}) do |word, hash|
+    #   hash[word] = score(word)
+    # end
+    # sorted_words = word_hash.sort_by {|word, score| score}
+    # sorted_words.last.first
+  #   words.max_by { |word| score(word)}
+  # end
+
+  # def score
+  #   letters.reduce(0) do |sum, letter|
+  #     sum + scores[letter]
+  #   end
+  # end
+
+  # def letters
+  #   word.to_s.upcase.split("")
+  # end
+
   def self.score(word)
-    0
+    letters = word.to_s.upcase.split("")
+    letters.reduce(0) { |sum, letter| sum += scores[letter] }
   end
+
+  def self.highest_score_from(words)
+    highest_word = words.sort_by { |word| score(word) }.last
+    highest_words = words.select{ |word| score(word) == score(highest_word)}
+    highest_words.sort_by { |word| word.length}.last  
+  end
+
+  def self.scores
+    {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
+  end
+
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -4,8 +4,44 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class ScrabbleTest < Minitest::Test
-  def test_it_can_score_a_single_letter
-    assert_equal 1, Scrabble.score("a")
-    assert_equal 4, Scrabble.score("f")
+
+  def test_can_score_word
+    assert_equal 8, Scrabble.score("hello")
+    assert_equal 5, Scrabble.score("ape")
   end
+
+  def test_blank_string_is_0_points
+    assert_equal 0, Scrabble.score("")
+  end
+
+  def test_nil_is_zero
+    assert_equal 0, Scrabble.score(nil)
+  end
+
+  def test_highest_score
+    assert_equal "home", Scrabble.highest_score_from(['home', 'word', 'hello', 'sound'])
+  end
+
+  def test_highest_score_tie_but_one_word_seven_letters
+    assert_equal "silence", Scrabble.highest_score_from(['home', 'word', 'silence'])
+  end
+
+  # def test_it_can_score_a_single_letter
+  #   assert_equal 1, Scrabble.score("a")
+  #   assert_equal 4, Scrabble.score("f")
+  # end
+
+  # def test_it_can_score_a_word
+  #   assert_equal 5, Scrabble.score("ape")
+  # end
+
+  # def test_returns_zero_for_empty_string
+  #      assert_equal 0, Scrabble.score("")
+  #   assert_equal 0, Scrabble.score(nil)
+  # end
+
+  # def test_highest_score_from
+  #   assert_equal "home", Scrabble.highest_score_from(['home', 'word', 'hello', 'sound'])
+  #   assert_equal "cabbage", Scrabble.highest_score_from(['cabbage', 'sound', 'hello'])
+  # end
 end
